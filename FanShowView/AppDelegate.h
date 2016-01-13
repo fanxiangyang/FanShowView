41a7042c873b623128b27ce48969579233499a7e
fanxiangyang <fanxiangyang_heda@163.com>
2016-01-13 16:10:41 +0800

    Initial Commit

---
+++ FanShowView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FanShowView
+//
+//  Created by 向阳凡 on 16/1/13.
+//  Copyright © 2016年 凡向阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FanShowView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FanShowView
+//
+//  Created by 向阳凡 on 16/1/13.
+//  Copyright © 2016年 凡向阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
